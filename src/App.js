7ce142d11f04cbc2217ca63904728801f09d8d4d
Mathaus <mathausannibal@gmail.com>
2021-03-09 19:10:54 -0300

    adiciona feat-req3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ProductList } from './pages';
+import { ShoppingCart } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ ProductList } />
+        <Route path="/meucarrinho" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );

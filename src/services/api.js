bb3ea8c392706a3d8131fe5dfde52cb2d45f01f9
Matheus Martino <matheus.martino97@gmail.com>
2021-03-09 19:50:20 -0300

    [Grupo-19] adicionado requisito 5.

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebeY
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const endpointRequest = await fetch(endpoint);
   const endpointResponse = await endpointRequest.json();
   return endpointResponse;

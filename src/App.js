ef3118529a6d261356482bd3e368fc17f265eafc
Matheus Martino <matheus.martino97@gmail.com>
2021-06-03 15:33:25 -0300

    Criando gh page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter as BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home, ShoppingCart, Details } from './pages';
 import './App.css';
 

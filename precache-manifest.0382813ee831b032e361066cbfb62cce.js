cc680b272e2a1b7421190d211ed30ceeafae2498
Matheus Martino <matheus.martino97@gmail.com>
2021-06-03 15:33:49 -0300

    Updates

---
+++ precache-manifest.0382813ee831b032e361066cbfb62cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c085b67b2eec569b4541973d39f040",
+    "url": "/17_sd-09-project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "f89c08a96090cf908d30",
+    "url": "/17_sd-09-project-frontend-online-store/static/css/main.22edfa0f.chunk.css"
+  },
+  {
+    "revision": "e2ac38794889ecbd9ad0",
+    "url": "/17_sd-09-project-frontend-online-store/static/js/2.2dbdbaa7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/17_sd-09-project-frontend-online-store/static/js/2.2dbdbaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89c08a96090cf908d30",
+    "url": "/17_sd-09-project-frontend-online-store/static/js/main.b9a945a3.chunk.js"
+  },
+  {
+    "revision": "8a4f330fbb81ebc2a8e0",
+    "url": "/17_sd-09-project-frontend-online-store/static/js/runtime-main.0a2d4fb4.js"
+  }
+]);
